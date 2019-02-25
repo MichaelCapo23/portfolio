4895030542c0a8fe19ac162646b78323e2fdcaaf
MichaelCapo23 <123pie23@gmail.com>
2019-02-25 12:44:47 -0800

    moved debugger

--- js/scripts.js
+++ js/scripts.js
@@ -177,6 +177,9 @@ $(window).on("load", function () {
                         }
                     }
                 });
+                var messageAlert = 'alert-' + data.type;
+                var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + "Unable To Send Email" + '</div>';
+                $('#contact-form').find('.messages').html(alertBox);
                 return false
             } catch {
                 var messageAlert = 'alert-' + data.type;
