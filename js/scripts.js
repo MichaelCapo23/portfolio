00eb5b403792ea2e906c1802eee20c2ccbde26e3
MichaelCapo23 <123pie23@gmail.com>
2019-02-25 12:53:32 -0800

    moved debugger

--- js/scripts.js
+++ js/scripts.js
@@ -176,9 +176,6 @@ $(window).on("load", function () {
                         }
                     }
                 });
-                var messageAlert = 'alert-' + data.type;
-                var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + "Unable To Send Email" + '</div>';
-                $('#contact-form').find('.messages').html(alertBox);
             } catch {
                 var messageAlert = 'alert-' + data.type;
                 var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + "Unable To Send Email" + '</div>';
