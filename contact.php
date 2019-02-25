bc944474bba99b6850778bc4fef08afcb322c3e0
MichaelCapo23 <123pie23@gmail.com>
2019-02-25 11:33:24 -0800

    fixed php conatct

--- contact.php
+++ contact.php
@@ -1,39 +1,53 @@
 <?php
 
 $from = '<demo@domain.com>';
-$sendTo = '<michaelacapo23@gmailcom>';
-$subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-try {
-    $emailText = "You have new message from contact form\n=============================\n";
-    foreach ($_POST as $key => $value) {
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
 
-    }
+$sendTo = '<michaelacapo23@gmail.com>';
+
+$subject = 'New message from contact form';
+
+$fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+$errorMessage = 'There was an error while submitting the form. Please try again later';try
+
+{
+
+    $emailText = "You have new message from contact form\n=============================\n";    foreach ($_POST as $key => $value) {        if (isset($fields[$key])) {
+
+    $emailText .= "$fields[$key]: $value\n";
+
+}
+
+}
+
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
+
         'From: ' . $from,
+
         'Reply-To: ' . $from,
+
         'Return-Path: ' . $from,
-    );
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-} catch (\Exception $e) {
+
+    );    mail($sendTo, $subject, $emailText, implode("\n", $headers));    $responseArray = array('type' => 'success', 'message' => $okMessage);
+
+}
+
+catch (\Exception $e)
+
+{
+
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+
+}if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+    $encoded = json_encode($responseArray);    header('Content-Type: application/json');    echo $encoded;
+
 }
-//catch (\SoapFault $e) {}
-try {
-    if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-        $encoded = json_encode($responseArray);
-        header('Content-Type: application/json');
-        echo $encoded;
-    } else {
-        echo $responseArray['message'];
-    }
-} catch (\SoapFault $e) {
+
+else {
+
     echo $responseArray['message'];
-}
-?>
+
+}?>
